a0e389f2ecd1894b851cd891ed4b4a4fb61feb04
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2023-12-08 22:38:37 +0000

    Added Main class.

---
+++ src/main/java/software/ulpgc/kata4/Main.java
@@ -0,0 +1,15 @@
+package software.ulpgc.kata4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:organizations.db")) {
+            for (Track track : SqliteTrackLoader.with(connection).loadAll())
+                System.out.println(track);
+        }
+    }
+
+}
