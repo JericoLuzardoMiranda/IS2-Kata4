46e7b9db1c5943e6c9d74f9f2c907b0943bedfda
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2023-12-08 22:36:36 +0000

    Added Track record.

---
+++ src/main/java/software/ulpgc/kata4/Track.java
@@ -0,0 +1,5 @@
+package software.ulpgc.kata4;
+
+public record Track(int id, String name, String website, String country, String description, int founded,
+                    String industry, int numberOfEmployees) {
+}
